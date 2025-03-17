499b23c41d460e232fc5f8cd401b941bc1f781d9
Jennipher <jenniphermedina11@gmail.com>
2025-03-17 16:59:18 -0500

    added more to example

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+    //display alert "good afternoon"
+    window.alert("Good afternoon")
+
+    //declare the variable to hole the div element
+    let container = document.getElementById("container");
+    
+    //getElementID is the method to locate the element by the ID
+    //container is an HTML element, it is in the div 
+
+    //set the content of element 
+    container.innerHTML = "This is the container place";
+
+    //change color style to red
+    container.style.color="red";
+
+    document.write("<p>This is a new paragraph</p>");
