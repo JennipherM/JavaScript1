6cd80f88666b0f583acf6b963fbc39df0aaad5d0
Jennipher <jenniphermedina11@gmail.com>
2025-03-24 12:31:19 -0500

    added to example

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
+//display alert "good afternoon"
+window.alert("Good afternoon")
 
+//declare the variable to hole the div element
+let container = document.getElementById("container");
 
-    //display alert "good afternoon"
-    window.alert("Good afternoon")
+//getElementID is the method to locate the element by the ID
+//container is an HTML element, it is in the div 
 
-    //declare the variable to hole the div element
-    let container = document.getElementById("container");
-    
-    //getElementID is the method to locate the element by the ID
-    //container is an HTML element, it is in the div 
+//set the content of element 
+container.innerHTML = ("<p>This is the container place</p>");
 
-    //set the content of element 
-    container.innerHTML = "This is the container place";
+//change color style to red
+container.style.color = "red";
 
-    //change color style to red
-    container.style.color="red";
+document.write("<p>This is a new paragraph</p>");
 
-    document.write("<p>This is a new paragraph</p>");
+//declare a variable to hold header
+let header = document.getElementsByTagName("h1")[0];
+let footer = document.getElementsByTagName("h1")[1];
+
+//declare a new variabl eto hole button element
+let btnClickMe = document.getElementById("btnClickMe");
+
+btnClickMe.onclick = function () 
+{
+    //window.alert("Hello");
+    header.style.color = "red";
+    footer.style.color = "blue";
+    container.style.visibility = "hidden";
+}
+
+//handdel the move over event the header
+header.onmouseover = function () 
+{
+    header.style.color = "red";
+}
+
+//handle the mouse oout event the header
+header.onmouseout = function () 
+{
+    header.style.color = "black";
+}
+
+//declare a variable to work with the text input username
+let username = document.getElementById("username");
+
+username.onchange = function()
+{
+    container.innerHTML = username.value;
+}
