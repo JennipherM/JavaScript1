d750320e9dd070b9648025be9e278a42b92c7e88
Jennipher <jenniphermedina11@gmail.com>
2025-03-24 16:58:41 -0500

    added changed

--- script.js
+++ script.js
@@ -20,23 +20,36 @@ let header = document.getElementsByTagName("h1")[0];
 let footer = document.getElementsByTagName("h1")[1];
 
 //declare a new variabl eto hole button element
+//butClickMe is an HTLM element, an object
+//getElementByID is a method of a document object
 let btnClickMe = document.getElementById("btnClickMe");
+let img = document.getElementById("image");
 
-btnClickMe.onclick = function () 
+//when the button is clicked, the header changes to red, footer turns blue, and container disappears
+btnClickMe.onclick = function ()  //onclick is a property
 {
     //window.alert("Hello");
     header.style.color = "red";
     footer.style.color = "blue";
-    container.style.visibility = "hidden";
+    //container.style.visibility = "hidden";
+    img.src = "flower.png";
+   
+    //assign text to input element
+
+    //assign value to input element to out element; turns the container text into what is input in the username box
+    container.innerText = username.value
+
+   // username.value=container.innerText;
+   // container.value="some text";  //change the value of input text
 }
 
-//handdel the move over event the header
+//handle the move over event the header; when mouse goes over header, it turns red
 header.onmouseover = function () 
 {
     header.style.color = "red";
 }
 
-//handle the mouse oout event the header
+//handle the mouse out event the header; when the mouse leaves the header, it turns to black
 header.onmouseout = function () 
 {
     header.style.color = "black";
@@ -49,3 +62,58 @@ username.onchange = function()
 {
     container.innerHTML = username.value;
 }
+
+/*
+5 ways to associate a handle to events
+
+1. inline association 
+    EX:  <button id="btnClickMe" onclick = "window.alert('hello')">Click Me</button>
+
+2. associate the onclick to an anonymous function
+    EX:  btnClickMe.onclick = function ()
+         {
+            window.alert("Hello");
+         }
+
+3. associate with a non-anonymous (or named) function to onclick
+    EX:  function handleClick()
+        {
+            window.alert('clicked');
+        }
+        btnClickMe.onclick = handleClick;
+
+4. use the method addEventListener() to associate handle
+    EX: document.addEventListener('click', handleClick);
+        function handleClick()
+        {
+          window.alert('clicked');
+        }
+
+5. arrow function
+    EX: btnClickMe.onclick = ()=>{window.alert('clicked');}
+*/
+
+
+/*
+Local vs Global Variables
+
+//declare global variable
+let x =5;
+const PI = 3.14 //declare a global constant
+
+let emptyString = "";
+let specialString = "I'm fine"
+
+btnClickMe.onclick = handleClick;
+function handleClick()
+{
+    let y = 5;  //local variable; cannot be accessed outside on handleClick
+    window.alert(x);
+}
+function handleClick2()
+{
+    let y = 5;
+    window.alert(y);  //error, cant accessed y since it is not a global variable, like how x is
+    window.alert(x);  //valid, x is global
+}
+*/
